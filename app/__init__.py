5eaf47e4746f3c588056946aa8520a95a407e633
Luzivam34 <luzivam.santos28@outlook.com>
2025-07-15 16:45:41 -0300

    app roudando e com acesso ao banco de dados

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,17 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from config import Config
+from flask_migrate import Migrate
 
 db = SQLAlchemy()
+
 
 def create_app():   
     app = Flask(__name__)
     app.config.from_object(Config)
     
     db.init_app(app)
+    migrate = Migrate(app,db)
 
     with app.app_context():
         from app.models.clube import Clube  # Import routes to register them
